b798d01293ba52e9790059cdd49f75276a72c3a0
Isaac Hair <isaacmhair@gmail.com>
2020-07-07 02:46:18 -0700

    emulator constructor and main methods are written

--- emulator.java
+++ emulator.java
@@ -1,31 +1,29 @@
 import java.io.*;
 import java.util.*;
-class emulator {
-	public static void main(String [] args) throws Exception {
-		if (args.length != 1) {
-			System.out.println("bitch");
-			System.exit(0x01);
-		}
-		FileInputStream in = new FileInputStream(args[0]);
-		emulator machine = new emulator(in);
-		while (1) {
-			machine.cycle();
-			System.out.print("\033[2J");
-			System.out.println("current: "+machine.current());
-			System.out.println("next: "+machine.next());
-			System.out.println("instruction: "+machine.inst()+"\n");
-			System.out.println("addr: "+machine.addr());
-			System.out.println("register: "+machine.reg());
-			System.out.println("direction: "+machine.dir());
-			System.out.println("out reg: "+machine.outreg());
-			System.out.println("output: "+machine.output());
-		}
-	}
-	private static short ram[];
-	private static byte inst[][];
-	emulator(FileInputStream in) throws Exception {
-		ram = new short[0x10000];
-		inst = new byte[0x10000][5];
-		while (1) {
 
+class emulator {
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("provide a rom file you fuck");
+			return;
+		}
+		emulator machine = new emulator(args[0]);
 	}
+	private byte rom[0x10000][5];
+	private short ram[0x10000];
+	private short curr;
+	private short nxt;
+	private short addr;
+	private short dir;
+	private short out;
+	private short reg;
+	public emulator(String sourceName) {
+		rom = new byte[0x10000][5];
+		ram = new short[0x10000];
+		FileInputStream source = new FileInputStream(sourceName);
+		curr = 0x0000;
+		for (int i = 0; source.read(rom[i]) > 0; i++)
+			;
+		source.close();
+	}
+}
